4c1fed195586104761833a299df04b740f6916a6
Shubham-Udawant <udawantshubham2015@gmail.com>
2025-11-05 16:22:56 +0530

    added key pair

--- ec2-instance-setup.tf
+++ ec2-instance-setup.tf
@@ -21,7 +21,10 @@ resource "aws_instance" "ec2_example" {
   vpc_security_group_ids = [aws_security_group.sg_vpc_jhooq_eu_central_1.id]
 }
 
-
+resource "aws_key_pair" "deployer" {
+  key_name   = "aws_key"
+  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDpa9rzBWqZ8ecRkDvDJa84WMe9UaxaPmb5IO3OQ7ewKIPqug53rAse/wSUT1QwPLHGraKKkKx26NOiX7KMxRLNLgVgrHVWtNDXZ5eKuDgP2+6DJMeWlKgdiQhalzhbNvkSAGTUvrUehKyNcjdD70UGK8hW8ckL+8W0oV59bhcoqhQkjN0OvDgGzxiY+n1PuSTMNui9bRTNrrUGogWSyjyE1F/t5NfwMT5Qi+Erxky5Jf81AXEQuPpgl08v9rtGxIS0rfDXxxhdHtdAs7CHjvF5g2ppPP4eLa2zR/qThK9ncWapT7v/vKBz6sTSLkXky08n3eqbjMeRdAg7jBzhWWy5 shubham@Shubhams-MacBook-Air.local"
+}
 
 
 /*output "fetched_info_from_aws" {
